7302d657372157a06694ada4dad8448622e33a1e
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-08 17:46:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -15,12 +15,12 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [
-        {:name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"},
-        {:name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"}
+        {name: "Benvolio",
+        age: "17",
+        attitude: "worried"},
+        {name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
@@ -38,12 +38,12 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [
-        {:name => "Steven",
-        :age => "30",
-        :attitude => "confused"},
-        {:name => "Nurse",
-        :age => "44",
-        :attitude => "worried"}
+        {name: "Steven",
+        age: "30",
+        attitude: "confused"},
+        {name: "Nurse",
+        age: "44",
+        attitude: "worried"}
         ]
    }
   }
